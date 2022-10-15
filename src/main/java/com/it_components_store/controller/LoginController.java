863c921bc847160b1bae9be69f2e414f2b11447a
Stanciulescu Gabriel <gabriel.stanciulescu08@gmail.com>
2022-10-15 21:51:48 +0300

    I created the registration for the user interface and authentication

---
+++ src/main/java/com/it_components_store/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.it_components_store.controller;
+
+import com.it_components_store.entity.User;
+import com.it_components_store.service.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final UserServiceImpl userService;
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        User user = new User();
+        model.addAttribute("user",user);
+        return "register";
+    }
+
+    @PostMapping("/registerUser")
+    public String userRegister(@ModelAttribute("user") User user){
+        userService.addUsers(user);
+         return "redirect:/login";
+    }
+
+
+
+
+
+}
--- src/main/java/com/it_components_store/entity/User.java
+++ src/main/java/com/it_components_store/entity/User.java
@@ -2,13 +2,8 @@ package com.it_components_store.entity;
 
 import java.time.LocalDate;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -47,7 +42,7 @@ public class User {
 
     private String mobile;
 
-    @ManyToOne
+    @ManyToOne()
     @JoinColumn(name = "id_role")
     private Role role;
 
