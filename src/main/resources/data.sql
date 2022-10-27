1bc6480899eea342aab3860b6e44f6edb4cd0764
Stanciulescu Gabriel <gabriel.stanciulescu08@gmail.com>
2022-10-27 20:16:02 +0300

    Update page of product

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -63,8 +63,47 @@ INSERT INTO products(code, description, image_url, name, price, quantity, id_cat
         ,'SSD',23,100, 4,100),
         ('F4-2133C15S-4GNT','SSD ADATA Legend 710 512GB PCI Express 3.0 x4 M.2 2280','https://5.grgs.ro/images/products/1/8221/2369816/thumbnails/adata-ssd-512gb-m2-pcie-legend-710-51b395e5a46db07e2c400251b165e70e.jpg'
         ,'SSD',23,100, 4,100),
-        ('F4-2133C15S-4GNT','SSD Samsung 980 250GB PCI Express 3.0 x4 M.2 2280','https://5.grgs.ro/images/products/1/4167/2180346/thumbnails/980-250gb-pci-express-30-x4-m2-2280-c370a8eafb1905dcea9fad9b8708f095.jpg'
-        ,'SSD',23,100, 4,100);
+        ('F4-2133C15S-4GNT','SSD Samsung 980 250GB PCI Express 3.0 x4 M.2 2280','https://4.grgs.ro/images/products/1/9395/2136430/thumbnails/870-evo-1tb-sata-iii-25-inch-13fcd8e5f2722e40e2e96e8a49645f13.jpg'
+        ,'SSD',23,100, 4,100),
+
+        ('F4-2133C15S-4GNT','Procesor Intel Comet Lake, Celeron G5925 3.6GHz box','https://5.grgs.ro/images/products/1/6924/2120514/thumbnails/comet-lake-celeron-g5925-36ghz-box-40dc6a717ec5260d90767dd588c86038.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor AMD Ryzen 3 4100 3.8GHz box','https://1.grgs.ro/images/products/1/8387/2364764/thumbnails/100-100000510box-d894366c13c426d11fc44750ec87375e.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor Intel Comet Lake, Core i3 10105F 3.7GHz box','https://3.grgs.ro/images/products/1/7201/2159338/thumbnails/comet-lake-core-i3-10105f-37ghz-box-42cdd95fe8e16e69be98b672cbcad3dc.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor AMD Ryzen 3 3200G 3.6GHz box','https://4.grgs.ro/images/products/1/1275/1893902/thumbnails/ryzen-3-3200g-36ghz-box-c0261fadda251d55fc4d46d73351dc23.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor Intel Comet Lake, Core i5 10400 2.9GHz box','https://4.grgs.ro/images/products/1/3041/2038114/thumbnails/comet-lake-core-i5-10400-29ghz-box-c1cefffcc66e9a3d33c171d6c730c2ba.png'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor Intel Alder Lake, Core i5 12400F 2.5GHz box','https://5.grgs.ro/images/products/1/6878/2310274/thumbnails/alder-lake-core-i5-12400f-25ghz-box-d370ccc46c4a739096347df3521b757b.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor AMD Ryzen 7 5800X 3.8GHz box','https://1.grgs.ro/images/products/1/9507/2102402/thumbnails/ryzen-7-5800x-38ghz-box-1b0292af425c0b74ea80f05f455e1b87.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor AMD Ryzen 3 1200 3.1GHz Tray','https://4.grgs.ro/images/products/1/1363661/2287398/thumbnails/ryzen-3-1200-31ghz-tray-43a91ff00b119d3aa0edf0381586e314.jpg'
+        ,'CPU',23,100, 5,100),
+        ('F4-2133C15S-4GNT','Procesor AMD Ryzen 7 5700X 3.4GHz box','https://4.grgs.ro/images/products/1/9544/2355140/thumbnails/amd-ryzen-7-5700x-box-169e6f569506a5acf930abef5324d2db.jpg'
+        ,'CPU',23,100, 5,100),
+
+
+        ('F4-2133C15S-4GNT','Placa video GIGABYTE GeForce GTX 1650 OC 4GB GDDR5 128-bit','https://5.grgs.ro/images/products/1/1848774/1857054/thumbnails/geforce-gtx-1650-oc-4gb-gddr5-128-bit-7a2cc05bff396cd62b251d432f8aeac2.png'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video Palit GeForce GTX 1650 GP 4GB GDDR6 128-bit','https://1.grgs.ro/images/products/1/1034/2043374/thumbnails/vga-palit-gtx1650-gamingpro-4g-gddr6-dp2-b064ed77886ce763bbed80a8a0d271d2.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video Gainward GeForce RTX 3060 Ghost LHR 12GB GDDR6 192-bit','https://3.grgs.ro/images/products/1/7958/2147094/thumbnails/geforce-rtx-3060-ghost-lhr-12gb-gddr6-192-bit-10919669f9935ebf7f292daf6a43b8cd.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video GIGABYTE GeForce RTX 3060 EAGLE LHR 12GB GDDR6 192-bit','https://3.grgs.ro/images/products/1/5113/2241666/thumbnails/geforce-rtx-3060-eagle-lhr-12gb-gddr6-192-bit-2a8f848de5e2d8fdafa50570a998fdb5.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video Palit GeForce RTX 3060 Ti Dual LHR 8GB GDDR6 256-bit','https://1.grgs.ro/images/products/1/2118138/2118506/thumbnails/geforce-rtx-3060-ti-dual-8gb-gddr6-256-bit-9f0c838e1a637cee7e8873083510a44a.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video Biostar Radeon RX 550 4GB GDDR5 128-bit','https://4.grgs.ro/images/products/1/7169/2230442/thumbnails/radeon-rx-550-4gb-gddr5-128-bit-196b221ce05febce704ae57c4c9c6ee8.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video MSI Radeon RX 6400 Aero ITX 4GB GDDR6 64-bit','https://1.grgs.ro/images/products/1/710/2377716/thumbnails/msi-radeon-rx-6400-aero-itx-4g-9c579fea7ac2d971d532325150c51ef6.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video ASUS GeForce RTX 3050 DUAL OC LHR 8GB GDDR6 128-bit','https://4.grgs.ro/images/products/1/9805/2334756/thumbnails/geforce-rtx-3050-dual-oc-lhr-8gb-gddr6-128-bit-63641329b2edb32c119a67d67c4275a9.jpg'
+        ,'GPU',23,100, 6,100),
+        ('F4-2133C15S-4GNT','Placa video MSI Radeon RX 6600 MECH 2X 8GB GDDR6 128-bit','https://3.grgs.ro/images/products/1/9551/2281094/thumbnails/radeon-rx-6600-mech-2x-oc-8gb-gddr6-128-bit-dfce7399853d457f07017f99266a8e86.jpg'
+        ,'GPU',23,100, 6,100);
 
 
 
